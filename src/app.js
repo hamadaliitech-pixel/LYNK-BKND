0afdc5a8bf88afb84232c4548a1bd2d8b0b1a498
user.name :"hammad <hamadalii.tech@gmail.com>
2026-09-25 21:56:45 +0500

    cors update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Allow frontend communication (cross-origin requests)
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://lynk-inky.vercel.app",
   credentials: true
 }));
 
