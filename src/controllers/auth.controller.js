aa09d7386c52e36f68bd3b32336a49b7a9d227f7
user.name :"hammad <hamadalii.tech@gmail.com>
2026-09-25 21:59:12 +0500

    JWT update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,7 +64,11 @@ async function LoginUser(req, res) {
     process.env.JWT_SECRET,
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
   res.status(200).json({
     message: "User login successfully",
