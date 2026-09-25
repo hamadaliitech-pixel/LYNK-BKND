a3a4e66f46d9e59b6e4c3026c6d86e687c5b22af
user.name :"hammad <hamadalii.tech@gmail.com>
2026-09-25 10:40:58 +0500

    first changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const app = require("./src/app");
 const connectdb = require("./src/db/db.js");
-const PORT = 3000;
 
-app.listen(PORT, async () => {
-  await connectdb();
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+async function start(){
+  connectdb()
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+start()
